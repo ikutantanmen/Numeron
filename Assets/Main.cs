55f8f9d71d54b28a4d616608f81f0ec58e7cf358
ikutashunya <s.ikuta1008@gmail.com>
2019-01-09 21:40:14 +0900

    Numeronプロジェクト

---
+++ Assets/Main.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+
+    // 式入力テキスト
+    public Text Formula;
+    // 結果表示テキスト
+    public Text Answer;
+    // 各数字ボタン
+    public Button[] bNumber;
+    // Callボタン
+    public Button bEqual;
+    // クリアボタン
+    public Button bClear;
+
+    // Use this for initialization
+    void Start () {
+        //初期化
+        Formula.text = "";
+        Answer.text = "";
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    // 数字ボタン押下
+    public void InputNumber(Text number){
+        // 押下したボタンの数字を式欄に追記する
+        Formula.text += number.text; 
+    }
+
+    // Callボタン押下
+    public void InputEqual(Text equal){
+
+    }
+
+    // クリアボタン押下
+    public void InputClear(Text equal){
+        //初期化
+        Formula.text = "";
+        Answer.text = "";        
+    }
+}
