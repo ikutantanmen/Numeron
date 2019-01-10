e31f963a0f2349d92eac3afeffae841f019a5bef
ikutashunya <s.ikuta1008@gmail.com>
2019-01-10 17:47:28 +0900

    1/10だいたい完成
    【今後の改善予定箇所】
    ・Formula.Textを3桁までしか入力できないようにする
    ・全体的なデザインの見栄えをよくする
    ・ゲームクリア時にリセットボタンを追加→答えの数字を再度ランダムに生成して遊べるようにする

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour {
 
@@ -15,12 +16,29 @@ public class Main : MonoBehaviour {
     public Button bEqual;
     // クリアボタン
     public Button bClear;
+    //答えとなる3桁の数字（x…百の位、y…十の位、z…一の位）
+    int x, y, z;
+    //プレイヤーが入力した3桁の数字（a…百の位、b…十の位、c…一の位）
+    int a = 0, b = 0, c = 0;
+    //eatの数、biteの数を表示する変数
+    int e_count, b_count;
 
     // Use this for initialization
     void Start () {
         //初期化
         Formula.text = "";
         Answer.text = "";
+
+        //答えの数字3桁をランダムに生成（同じ数字が2回以上でないようにする）
+        x = Random.Range(1, 10);
+        do
+        {
+            y = Random.Range(1, 10);
+        } while (y == x);
+        do
+        {
+            z = Random.Range(1, 10);
+        } while (z == x || z == y);
     }
 
     // Update is called once per frame
@@ -31,18 +49,73 @@ public class Main : MonoBehaviour {
     // 数字ボタン押下
     public void InputNumber(Text number){
         // 押下したボタンの数字を式欄に追記する
-        Formula.text += number.text; 
+        Formula.text += number.text;
+        if (a == 0)
+        {
+            a = int.Parse(number.text);
+        } else if (b == 0 && int.Parse(number.text)!= a)
+        {
+            b = int.Parse(number.text);
+        } else if (c == 0 && int.Parse(number.text) != b)
+        {
+            c = int.Parse(number.text);
+        }
     }
 
-    // Callボタン押下
-    public void InputEqual(Text equal){
 
+    // Callボタン押下
+    public void InputEqual(Text equal) {
+        //eatの数を測定
+        e_count = 0;
+        if (a == x)
+        {
+            e_count++;
+        }
+        if (b == y)
+        {
+            e_count++;
+        }
+        if (c == z)
+        {
+            e_count++;
+        }
+        //biteの数を測定
+        b_count = 0;
+        if (a == y || a == z)
+        {
+            b_count++;
+        }
+        if (b == x || b == z)
+        {
+            b_count++;
+        }
+        if (c == x || c == y)
+        {
+            b_count++;
+        }
+        if (e_count == 3)
+        {
+            Answer.text = "Hit!";
+        } else
+        {
+            Answer.text = e_count + "eat " + b_count + "bite" + "\r\n"
+                + "あなたの数字：" + a + "" + b + "" + c + "\r\n"
+                + "答え：" + x + "" + y + "" + z;
+            //再度初期化
+            Formula.text = "";
+            a = 0;
+            b = 0;
+            c = 0;
+        }
     }
 
     // クリアボタン押下
     public void InputClear(Text equal){
         //初期化
         Formula.text = "";
-        Answer.text = "";        
+        Answer.text = "";
+        a = 0;
+        b = 0;
+        c = 0;
     }
 }
